ed8156410849e56d49dc62770cd3afdbb13ebebc
Prox <prox.darknet@gmail.com>
2020-08-08 20:35:23 -0400

    Quick update to write out both the unique and non unique wordlists along with sripping a space and colon that was presnt

--- WebScraper.py
+++ WebScraper.py
@@ -3,6 +3,7 @@ import requests
 import string
 from html.parser import HTMLParser
 
+
 class MyHTMLParser(HTMLParser):
     def __init__(self, required_tag, required_value):
         HTMLParser.__init__(self)
@@ -16,41 +17,49 @@ class MyHTMLParser(HTMLParser):
             for name, value in attrs:
                 if value == self.required_value:
                     self.recording = 1
+
     def handle_endtag(self, tag):
         if tag == self.required_tag and self.recording:
-            self.recording -=1
+            self.recording -= 1
+
     def handle_data(self, data):
         if self.recording:
             self.data.append(data)
 
+
 base_url = 'https://www.SlangSite.com/slang/'
 end_url = '.html'
-#Array to hold all of the words
+# Array to hold all of the words
 words = []
 
-#Loop through the letters of the alphabet
+# Loop through the letters of the alphabet
 for letter in string.ascii_uppercase:
-    #Something, something, the word your looking for is pagination
+    # Something, something, the word your looking for is pagination
     page = 1
 
     url_req = requests.get(f'{base_url}{letter}{end_url}')
-    #Read out the div tags that contains the words
+    # Read out the div tags that contains the words
     columnist_parse = MyHTMLParser('font', '3')
     columnist_parse.feed(url_req.text)
-    #Loop through the words and make sure they actually exist, add them to array
+    # Loop through the words and make sure they actually exist, add them to array
     for columnist in columnist_parse.data:
         if columnist != '\n' and len(columnist) >= 1:
             words.append(columnist)
     columnist_parse.close()
 
-print("number of words: "+str(len(words)))
+print("number of words: " + str(len(words)))
 
 unique_words = list(set(words))
 
-print("number of unique words: "+str(len(unique_words)))
+print("number of unique words: " + str(len(unique_words)))
 
-#Create a file and write all of the words to it
-# word_list = open ("SlangSite.txt", "w")
-# for word in words:
-#     word_list.write(word + '\n')
-# word_list.close()
+# Create a file and write all of the words to it
+word_list = open ("SlangSite.txt", "w")
+for word in words:
+    word_list.write(word[:-2] + '\n')
+word_list.close()
+
+word_list = open ("SlangSiteUnique.txt", "w")
+for word in unique_words:
+    word_list.write(word[:-2] + '\n')
+word_list.close()
